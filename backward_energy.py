b4d3b3ce2bbb95580da7285b1b6b2b72b8b36531
Afra Antara Anjum <76681973+afraAntara@users.noreply.github.com>
2023-12-11 01:00:24 -0800

    Add files via upload

--- backward_energy.py
+++ backward_energy.py
@@ -8,40 +8,38 @@ from PIL import Image
 from numba import jit
 from PyQt5.QtGui import QImage, QImageReader
 
+# Initialize and set values for filters fx and fy
+filter_x = np.array([[-1, 0, 1],
+                     [-2, 0, 2],
+                     [-1, 0, 1]])
+
+filter_y = np.array([[-1, -2, -1],
+                     [0, 0, 0],
+                     [1, 2, 1]])
 
 @jit
 def compute_energy(image):
 
-    rows, cols = image.shape[:2]
+    rows, columns = image.shape[:2]
+    gray_image = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2GRAY).astype(float)
+    energy = np.zeros((rows, columns))
 
-    I = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2GRAY).astype(float)
-    energy = np.zeros((rows, cols))
-    m = np.zeros((rows, cols))
+    gradient_x = np.zeros((rows, columns))  # Create numpy array gradient_x for Gx
+    gradient_y = np.zeros((rows, columns))  # Create numpy array gradient_y for Gy
+    gray_image = np.pad(gray_image, pad_width=1, mode='constant',constant_values=0)  # Add padding to grayscale image with all values surrounding as 0
 
-    up = np.roll(I, 1, axis=0)
-    left = np.roll(I, 1, axis=1)
-    right = np.roll(I, -1, axis=1)
+    # Initialize loop 1 to iterate through rows
+    for i in range(1, rows + 1):
+        # Initialize loop 2 to iterate through columns
+        for j in range(1, columns + 1):
+            # Carrying out element-wise multiplication followed by summing values for Gx and Gy
+            gradient_x[i - 1][j - 1] = np.sum(filter_x * gray_image[i - 1:i + 2, j - 1:j + 2])
+            gradient_y[i - 1][j - 1] = np.sum(filter_y * gray_image[i - 1:i + 2, j - 1:j + 2])
 
-    cost_Mid = np.abs(right - left)
-    cost_Left = np.abs(up - left) + cost_Mid
-    cost_Right = np.abs(up - right) + cost_Mid
-
-    for i in range(1, rows):
-
-            m_Up = m[i - 1]
-            m_Left = np.roll(m_Up, 1)
-            m_Right = np.roll(m_Up, -1)
-
-            top = np.array([m_Left, m_Up, m_Right])
-            costs = np.array([cost_Left[i], cost_Mid[i], cost_Right[i]])
-            top += costs
-
-            argmins = np.argmin(top, axis=0)
-            m[i] = np.choose(argmins, top)
-            energy[i] = np.choose(argmins, costs)
+    # Calculate energy using the Gx and Gy values, energy is the numpy array with the values
+    energy = np.sqrt(gradient_x ** 2 + gradient_y ** 2)
 
     return energy
-
 @jit
 def find_seam(image):
     energy = compute_energy(image)
@@ -68,13 +66,14 @@ def find_seam(image):
     # backtrack to find path
     seam_idx = []
     j = np.argmin(M[-1])
-
     # seam_energy=0
     for i in range(rows - 1, -1, -1):
         seam_idx.append(j)
         seam_energy += energy[i, j]
         j = backtrack[i, j]
+
     seam_idx.reverse()
+
     return np.array(seam_idx), seam_energy
 
 
@@ -106,25 +105,26 @@ def seam_carving(image, target_columns, target_rows):
     rows = 0
     cols = 0
     while rows <= target_rows or cols <= target_columns:
-        if (target_rows == 0):
+
+        if  (target_rows == 0):
             seam_vertical, col_energy = find_seam(image)
             image = remove_seam(image, seam_vertical)
             cols += 1
 
-        elif (target_cols == 0):
+        elif (target_columns == 0):
             image_rotate = rotate_image(image, True)
             seam_horizontal, row_energy = find_seam(image_rotate)
             image_rotate = remove_seam(image_rotate, seam_horizontal)
             image = rotate_image(image_rotate, False)
             cols += 1
-
+        
         else:
             seam_vertical, col_energy = find_seam(image)
             image_rotate = rotate_image(image, True)
-            seam_horizontal, row_energy = find_seam(image_rotate)
+            seam_horizontal, row_energy= find_seam(image_rotate)
             # print(row_energy,col_energy)
-            if col_energy < row_energy:
-                if cols <= target_columns:
+            if col_energy<row_energy:
+                if  cols<=target_columns:
                     image = remove_seam(image, seam_vertical)
                     cols += 1
                 else:
